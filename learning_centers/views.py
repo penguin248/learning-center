9b4c70c9f9b1871e18dbc63f733eb757f7d92f5b
James_Matara <jamesmatara11@gmail.com>
2024-05-31 08:12:53 +0300

    Add topic view to show all topics

--- learning_centers/views.py
+++ learning_centers/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """The home page for Learning Center"""
     return render(request, 'learning_centers/index.html')
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('sate_added')
+    context = {'topics': topics}
+    return render(request, 'learning_centers/topics.html', context)
