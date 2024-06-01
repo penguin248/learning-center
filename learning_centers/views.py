48fae9550105e9f04ef87b47e09240cb03b7558a
James_Matara <jamesmatara11@gmail.com>
2024-06-01 12:01:33 +0300

    Add new_topic view Add a a new_topic view function to handle initial requests for a new topic page and handle the processing of data submitted in the form

--- learning_centers/views.py
+++ learning_centers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """The home page for Learning Center"""
@@ -17,3 +18,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_centers/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        #No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        #POST Data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_centers:topics')
+    #Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_centers/new_topic.html', context)
+            
