d06aeda7f983b7ab9ecbaf0fb5feefddd2e7ae07
James_Matara <jamesmatara11@gmail.com>
2024-06-03 17:00:03 +0300

    Add a user logout view function Add user logout view function that handles the user logout process

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import logout
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'registration/logged_out.html', {})
+
