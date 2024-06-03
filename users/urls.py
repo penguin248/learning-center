d56801cc43c9953c1f53909740baaf7682c0aa44
James_Matara <jamesmatara11@gmail.com>
2024-06-03 17:05:30 +0300

    Modify the url path to include logout

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 """Defines URL patterns for users"""
 
 from django.urls import path, include
+from . import views
+
 
 app_name = 'users'
 urlpatterns = [
     #Include default auth urls
     path('', include('django.contrib.auth.urls')),
+    path('', views.user_logout, name = 'logout')
 ]
